13352a5e314dd4151336c38ba5f5649ea73bb5f3
Florencia Rodriguez <florencia.rz0719@gmail.com>
2024-06-28 14:20:59 -0300

    Tercer Version: Do While, If, Else.

--- src/juegoadivinar/Main.java
+++ src/juegoadivinar/Main.java
@@ -1,5 +1,6 @@
 package juegoadivinar;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +13,28 @@ public class Main {
 		int NumeroUsuario= 0;
 		int intentos = 0;
 		
-	}
+		System.out.println("\nADIVINA EL NÚMERO");
+		do {
+				
+				System.out.println(" Ingrese un Número del 1 al 10: ");
+				NumeroUsuario = scanner.nextInt();
+				intentos++;
+				
+				if (NumeroUsuario == NumeroAdivinar & intentos <5){
+					System.out.print("HAS ADIVINADO! EL NÚMERO ES " + NumeroAdivinar+ 
+					" LO HAS LOGRADO EN " +intentos+ " INTENTOS. ");}
+				
+				else {if (NumeroUsuario < NumeroAdivinar) {
+					System.out.println("El número debe ser mayor. Intenta otra vez. ");} 	
+				
+				else {if  (NumeroUsuario > NumeroAdivinar) {
+					System.out.println("El número debe ser menor. Intenta de nuevo. ");} }
+				}
+				}
+			
+			      while (NumeroUsuario != NumeroAdivinar & intentos<5);
+			      if (NumeroUsuario != NumeroAdivinar){
+			      System.out.print("HAS USADO TODOS LOS INTENTOS. EL NÚMERO ERA " +NumeroAdivinar);}
 
+}
 }
